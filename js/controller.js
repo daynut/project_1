9e5f8037cb115bbe1b67b802003c1193b1ffd379
Dana Olsen <danaolsen.info@gmail.com>
2016-09-14 12:48:50 -0400

    Submit W04D03 Project 1
    Status:
    Prompt window loads and collects player names.
    Cells are selected with mouseclick.
    
    Goal:
    Change cell color to player color when clicked.
    
    CSS
    Added empty player class and input class colors.
    
    HTML
    Changed aside to div to get prompt window working.
    Added class and id properties to input with span tags.
    
    JS-Controller
    Added x and y positions and grabbed them.
    Put in placeholder alert if position is taken.
    Put in empty changePlayer function.
    Remembered to commit prior changes.
    
    JS-Function
    Made function to add player color to board.
    Needs to grab player color.
    Needs to grab x and y pos.

--- js/controller.js
+++ js/controller.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 // console.log('controller is working');
 
 
-  config.redPlayerName = prompt("Please enter the first player's name. This player will use red game pieces.", config.redPlayerName) || config.redPlayerName;
-  config.blackPlayerName = prompt("Please enter the second player's name. This player will use black game pieces.", config.blackPlayerName) || config.blackPlayerName;
+  var $redPlayerName = prompt("Please enter the first player's name. This player will use red game pieces.", $redPlayerName) || $redPlayerName;
+  var $blackPlayerName = prompt("Please enter the second player's name. This player will use black game pieces.", $blackPlayerName) || $blackPlayerName;
 
   $('.prefix').text(config.playerPrefix);
   $('#player').addClass(currentPlayer).text(config[currentPlayer + "PlayerName"]);
@@ -13,10 +13,16 @@ $(document).ready(function(){
       var y_pos = $('.board tr').index($(this).closest('tr'));
       var x_pos = $(this).closest('tr').find('td').index($(this).closest('td'));
 
+      if (positionIsTaken(x_pos, y_pos)) {
+          alert(config.takenMsg);
+          return;
+      }
 
       addDiscToBoard(currentPlayer, x_pos, y_pos);
       printBoard();
 
+      changePlayer();
+  });
 
 
 
--- js/function.js
+++ js/function.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 
 
 
-$('#myTable').append('<tr><td>my data</td><td>more data</td></tr>');
+// $('#myTable').append('<tr><td>my data</td><td>more data</td></tr>');
+
+function addDiscToBoard(color, x_pos, y_pos) {
+    board[y_pos][x_pos] = color;
+}
 
 
 
