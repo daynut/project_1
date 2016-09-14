e917af3a5094aff77d66e02f05a67ebd8c9007cd
Dana Olsen <danaolsen.info@gmail.com>
2016-09-13 23:00:01 -0400

    Submit W04D02 Project 1
    CSS
    Put in button classes for red & black.
    
    HTML
    Put in an input and id for red player 1 (redName).
    
    JS
    Set up document and alert for redName input value.
    Tested keyup but need whole input entry, not first letter.
    Set onclick attribute in <input> in html.
    Tested it and am capturing input; looked up ways to capture inputs and do not want to do form.

---
+++ js/controller.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+// console.log('controller is working');
+
+  var $redName = $("#redName").val();
+
+    $("#redName").on("click", function(){
+      alert($(this));
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
