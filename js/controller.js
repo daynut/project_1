6c6e8a4f0985c0fb04f0beecc715876334d04d61
Dana Olsen <danaolsen.info@gmail.com>
2016-09-14 09:24:18 -0400

    Submit W04D02 Project 1
    CSS
    Goal: capture player names.
    Put in an aside bar for a form.
    
    HTML
    Started, then abandoned input and form, in the interest of time mgmt.
    Trying to use span tags to capture values.
    
    JS
    Testing syntax seen on Stackoverflow to set the player names with a prompt.
    Doesn't work as expected.
    What am I missing...

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 // console.log('controller is working');
 
-  var $redName = $("#redName").val();
+  // var $redName = $("#redName").val();
+  //
+  //   $("#redName").on("click", function(){
+  //     alert($(this));
+  //     console.profile($(this));
+  //   });
 
-    $("#redName").on("click", function(){
-      alert($(this));
-    });
-
-
+config.redPlayerName = prompt("Please enter the first player's name. This player will use red game pieces.", config.redPlayerName);
+config.blackPlayerName = prompt("Please enter the second player's name. This player will use black game pieces.", config.blackPlayerName);
 
 
 
