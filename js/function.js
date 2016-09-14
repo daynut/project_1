6cb73fb81db579434e57f51e570ba341f0815dc1
Dana Olsen <danaolsen.info@gmail.com>
2016-09-14 13:35:09 -0400

    Submit W04D03 Project 1
    Goal:
    Keep making progress.
    If stuck, start with what I know and can do.
    
    JS
    Made function to change players.

--- js/function.js
+++ js/function.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
 
-
-
-// $('#myTable').append('<tr><td>my data</td><td>more data</td></tr>');
-
-function addDiscToBoard(color, x_pos, y_pos) {
+  function addDiscToBoard(color, x_pos, y_pos) {
     board[y_pos][x_pos] = color;
-}
+  }
 
-
-
+  function changePlayer() {
+    if (currentPlayer === 'red') {
+        currentPlayer = 'black';
+    } else {
+        currentPlayer = 'red';
+    }
+  }
 
 
 
