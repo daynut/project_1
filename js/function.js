dd669b0fae4bbb21361ec7ffa1a8e32483392d70
Dana Olsen <danaolsen.info@gmail.com>
2016-09-14 20:48:19 -0400

    Submit W04D03 Project 1
    Goal:
    Select a cell on the board and change color.
    ABC (always be commiting)
    
    JS:
    Trying to define cell and add position class.

--- js/function.js
+++ js/function.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   function addDiscToBoard(color, x_pos, y_pos) {
     board[y_pos][x_pos] = color;
   }
+  $("your-pure-css-selector").eq(index)
+  var cell = 
+  cell.children('button').addClass(board[y][x]);
 
   function changePlayer() {
     if (currentPlayer === 'red') {
